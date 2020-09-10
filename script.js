343650b028fa756dc61ac08e3c08092dbb98e173
Rio_Nyx <58178550+Rio-Nyx@users.noreply.github.com>
2020-09-11 00:36:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+	var canvas = document.getElementById('myCanvas');
+	var ctx = canvas.getContext('2d');
+	window.addEventListener("load",()=>{
+		
+		createels();
+	});
+	
+	window.addEventListener("refresh",()=>{
+		canvas.height = window.innerHeight ;
+		canvas.width = window.innerWidth ;
+	});
+
+	function createels(){
+		
+		canvas.height = window.innerHeight ;
+		canvas.width = window.innerWidth ;
+
+		ctx.lineWidth = 3;
+		ctx.lineJoin = 'round';
+		ctx.lineCap = 'round';
+		ctx.strokeStyle = 'white';
+
+		var mouse = {x: 0, y: 0};
+		 
+		canvas.addEventListener('mousemove', function(e) {
+		  mouse.x = e.pageX - this.offsetLeft-8;
+		  mouse.y = e.pageY - this.offsetTop-8;
+		}, false);
+
+		 
+		canvas.addEventListener('mousedown', function(e) {
+			ctx.beginPath();
+			ctx.moveTo(mouse.x, mouse.y);
+			canvas.addEventListener('mousemove', onPaint, false);
+		}, false);
+		 
+		canvas.addEventListener('mouseup', function() {
+			canvas.removeEventListener('mousemove', onPaint, false);
+		}, false);
+		 
+		canvas.addEventListener('mouseleave',function(){
+			canvas.removeEventListener('mousemove', onPaint, false)
+			});
+		
+		var onPaint = function() {
+			ctx.lineTo(mouse.x, mouse.y);
+			ctx.stroke();
+		};
+		
+	}
+	
+	function openLeftMenu() {
+	  document.getElementById("leftMenu").style.display = "block";
+	}
+
+	function closeLeftMenu() {
+	  document.getElementById("leftMenu").style.display = "none";
+	}
+	
+	function fn1(){
+		console.log("erase fn");
+		ctx.strokeStyle="black";
+		ctx.lineWidth = 7;
+	}
+	
+	function fn2(){
+		ctx.strokeStyle="white";
+		ctx.lineWidth = 3;
+	}
+
+
